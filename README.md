75b40abd75d622770afa6c72b04acc51ab7c7d36
João Pedro <joaope.espindola2005@gmail.com>
2024-09-04 11:28:23 -0300

    install cors

---
+++ README.md
@@ -0,0 +1,27 @@
+# API Movies
+
+## 💻 About the project
+The idea is to create an application in Node.js where the user registers a film, fills in some information (name, description, rating) and creates tags related to it.
+
+## Learnings
+- HTTP Codes
+- Middlewares
+- SQL Commands
+- Knex (SQL Query Builder)
+- Migrations
+- Primary key and Foreign key
+- Inner join, Like operador, Where and WhereIn Filters, OrderBy
+- Bcryptjs - hash passwords (password encryption)
+
+## ⚙️ Technologies
+
+- JavaScript
+- NodeJS
+- Git e Github
+
+
+## Diagram
+<div align="left">
+  <img alt="Movies App UML" src="https://i.postimg.cc/432sR5zW/UML.png" width="800">
+</div>
+<br><br>
---
+++ src/controllers/SessionsController.js
@@ -0,0 +1,34 @@
+const knex = require("../database/knex")
+const AppError = require("../utils/AppError.js")
+const { compare } = require("bcryptjs")
+const authConfig = require("../configs/auth.js")
+const { sign } = require("jsonwebtoken")
+
+class SessionsController{
+  async create(request, response){
+    const { email, password } = request.body;
+
+    /*Verificação do email do usúario*/
+    const user = await knex("users").where({ email }).first();
+    if(!user){
+      throw new AppError("E-mail e/ou senha incorreta", 401);
+    }
+
+    /*Verificação da senha do usúario*/
+    const passwordMatched = await compare(password, user.password)
+    if(!passwordMatched) {
+      throw new AppError("E-mail e/ou senha incorreta", 401);
+    }
+
+    /*Geramento do Token do usuário*/
+    const { secret, expiresIn } = authConfig.jwt
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn
+    })
+
+    return response.json({ user, token})
+  }
+}
+
+module.exports = SessionsController;
